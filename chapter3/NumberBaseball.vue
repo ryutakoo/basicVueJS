54dbf4f2717c338fddd77717ec788f5c2913fd78
ryutakoo <ryu1861@gmail.com>
2020-05-22 20:37:50 +0900

    200522 챕터3 숫자야구 테스트 완료

--- chapter3/NumberBaseball.vue
+++ chapter3/NumberBaseball.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
-        <h1>{{ result }}</h1>
-        <h1>{{ answer }}</h1>
+        <h1>result {{ result }}</h1>
+        <!-- <h1>answer {{ answer }}</h1> -->
         <form v-on:submit="onSubmitForm">
             <input type="text"  minlength="4" maxlength="4" v-model="value" ref="answer">
             <button type="submit"> Button </button>
         </form>
         <ul>
-            <li v-for="i in tries" :key="i">{{i}}</li>
+            <li v-for="i in tries" v-bind:key="i.try">{{i}}</li>
         </ul>
         <div> 시도 {{tries.length}}</div>
     </div>
@@ -16,9 +16,15 @@
 <script>
 
 const getNumber = () => {
-    let array = new Array();
-    array.push(Math.floor(Math.random() * 10000))
-    return array
+    let candidates = ['1','2','3','4','5','6','7','8','9'];
+    let returnData = [];
+
+    for (let i =0; i < 4; i +=1 ){
+        const pickNumber = candidates.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+        returnData.push(parseInt(pickNumber));
+    };
+
+    return returnData; 
 };
 export default {
     data() {
@@ -32,7 +38,6 @@ export default {
     methods : {
         onSubmitForm(e) {
             e.preventDefault();
-            // console.log(`정답은 ${this.answer} 입니다`);
             console.log(this.value === this.answer.join(''))
             if(this.value === this.answer.join('')){
                 this.tries.push({
@@ -47,29 +52,33 @@ export default {
                 this.tries = [];
                 this.$refs.answer.focus();
             } else{
-                if(this.tries.length > 9){
+                if(this.tries.length >= 9){
                     alert('10회 만료되었습니다..게임을 재시작합니다.')
                     this.result = `10번 넘게 틀려 실패! 정답은 : ${this.answer} 입니다.`;
                 } else{
                     let strike = 0;
                     let ball = 0;
                     const answerArray = this.value.split('').map(v => parseInt(v));
-                    const resultArray=  this.answer.split('').map(v => parseInt(v));
-                    console.log(`answer : ${answerArray} result : ${resultArray}`);
-
-                    for (let i = 0; i < this.tries.length; i +=1 ){
-                        if(this.answerArray[i] === this.answer[i]){
+                      for (let i = 0; i < 4; i +=1 ){
+                        // console.log(`index : ${i}  answerArray[i] : ${answerArray[i]} this.answer[i] : ${this.answer[i]}`)
+                        // console.log(typeof(answerArray[i])    , typeof(this.answer[i]))
+                        // console.log('test' + this.answer.includes(answerArray[i]))
+                        // if(answerArray[i] === parseInt(this.answer[i])){
+                        if(answerArray[i] === this.answer[i]){
                             strike++;
-                        } else if(this.answer){
+                        } else if(this.answer.includes(answerArray[i])){
                             ball++;
                         }
                     }
+                    
                     this.tries.push({
                         try : this.value,
-                        result : ''
+                        result : `Strike : ${strike}  Ball : ${ball}`
                     })
-                    
                 }
+
+                this.value = ``;
+                this.$refs.answer.focus();
             }
            
         },
